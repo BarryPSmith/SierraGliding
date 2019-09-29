665a5a644d8620d059de4fee833b59f4cec3eb7b
ingalls <ingalls@protonmail.com>
2019-09-28 21:29:28 -0400

    Add readme files

---
+++ web/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const sqlite3 = require('sqlite3');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const router = new express.Router();
+
+app.disable('x-powered-by');
+
+app.use(express.static(path.resolve(__dirname, 'site')));
+app.use('/api/', router);
+
+router.use((req, res, next) => {
+    console.error(`[${req.method}] /api${req.url}`);
+    return next();
+});

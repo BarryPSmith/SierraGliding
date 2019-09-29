9ce622ea77a6b658f141f1dcf7577bdcecbf5b78
ingalls <ingalls@protonmail.com>
2019-09-28 21:44:04 -0400

    README updates

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1 align=center>Sierra Gliding Weather</h1>
+
+<p align=center>Low Interval Wind Station</p>
+
+
--- web/index.js
+++ web/index.js
@@ -8,10 +8,16 @@ const router = new express.Router();
 
 app.disable('x-powered-by');
 
-app.use(express.static(path.resolve(__dirname, 'site')));
+app.use(express.static(path.resolve(__dirname, 'site/dist')));
 app.use('/api/', router);
 
 router.use((req, res, next) => {
     console.error(`[${req.method}] /api${req.url}`);
     return next();
+});
+
+app.listen(4000, (err) => {
+    if (err) throw err;
+
+    console.error('Server listening http://localhost:4000');
 });

68df96b2ece4c5a9532cb6072bdd4c09ab791592
ingalls <ingalls@protonmail.com>
2019-09-29 01:30:05 -0400

    Modularize web server

--- web/index.js
+++ web/index.js
@@ -1,7 +1,14 @@
+#!/usr/bin/env node
+
+'use strict';
+
 const express = require('express');
 const sqlite3 = require('sqlite3');
-const fs = require('fs');
 const path = require('path');
+const args = require('minimist')(process.argv, {
+    string: ['db', 'port'],
+    boolean: ['help']
+});
 
 const app = express();
 const router = new express.Router();
@@ -11,33 +18,81 @@ app.disable('x-powered-by');
 app.use(express.static(path.resolve(__dirname, 'site/dist')));
 app.use('/api/', router);
 
-const db = new sqlite3.Database(path.resolve(__dirname, 'data.sqlite'));
 
-db.serialize(() => {
-    db.run(`
-        CREATE TABLE IF NOT EXISTS stations (
-            ID                  INT,
-            Name                TEXT,
-            Lon                 FLOAT,
-            Lat                 FLOAT,
-            Wind_Speed_Legend   TEXT,
-            Wind_Dir_Legend     TEXT
-        );
-    `);
+if (require.main === module) {
+    if (!args.db || args.help) {
+        return help();
+    }
 
-    db.run(`
-        CREATE TABLE IF NOT EXISTS station_data (
-            Station_ID          INT,
-            Timestamp           INT,
-            Windspeed           FLOAT,
-            Wind_Direction      FLOAT,
-            Battery_Level       Float
-        );
-    `);
+    database(path.resolve(__dirname, 'data.sqlite'), (err, db) => {
+        if (err) throw err;
 
-    return main(db);
-});
+        return main(db);
+    });
+} else {
+    module.exports = {
+        main: main
+    }
+}
 
+/**
+ * Print server CLI help documentation to STDOUT
+ *
+ */
+function help() {
+    console.log('');
+    console.log('  Start the Sierra Gliding Server');
+    console.log('');
+    console.log('  Usage ./index.js [--db <DB>] [--help]');
+    console.log('');
+    console.log('  Options:');
+    console.log('  --db <DB.sqlite>     [required] Backend Database to Create or Use');
+    console.log('  --port <PORT>        [optional] Choose an alternate port from the default 4000');
+    console.log('  --help               [optional] Print this message to stdout');
+    console.log('');
+    process.exit();
+}
+
+/**
+ * Instantiate a new Sqlite Database, creating schema is needed
+ *
+ * @param {String} dbpath Path to existing db or db to create
+ * @param {Function} cb Callback Function (err, db)
+ */
+function database(dbpath, cb) {
+    const db = new sqlite3.Database(dbpath);
+
+    db.serialize(() => {
+        db.run(`
+            CREATE TABLE IF NOT EXISTS stations (
+                ID                  INT,
+                Name                TEXT,
+                Lon                 FLOAT,
+                Lat                 FLOAT,
+                Wind_Speed_Legend   TEXT,
+                Wind_Dir_Legend     TEXT
+            );
+        `);
+
+        db.run(`
+            CREATE TABLE IF NOT EXISTS station_data (
+                Station_ID          INT,
+                Timestamp           INT,
+                Windspeed           FLOAT,
+                Wind_Direction      FLOAT,
+                Battery_Level       Float
+            );
+        `);
+
+        return cb(null, db);
+    });
+}
+
+/**
+ * Start the main Sierra Gliding webserver
+ *
+ * @param {Object} db Initialized Sqlite3 Database
+ */
 function main(db) {
     router.use((req, res, next) => {
         console.error(`[${req.method}] /api${req.url}`);
@@ -56,7 +111,7 @@ function main(db) {
 
     });
 
-    app.listen(4000, (err) => {
+    app.listen(args.port ? parseInt(args.port) : 4000, (err) => {
         if (err) throw err;
 
         console.error('Server listening http://localhost:4000');

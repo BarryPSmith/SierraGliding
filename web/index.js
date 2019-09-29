d2dfbbd10db2c234facdf66e89c05cdc3c228c6c
ingalls <ingalls@protonmail.com>
2019-09-29 01:04:40 -0400

    Add Station_Data tables

--- web/index.js
+++ web/index.js
@@ -11,13 +11,54 @@ app.disable('x-powered-by');
 app.use(express.static(path.resolve(__dirname, 'site/dist')));
 app.use('/api/', router);
 
-router.use((req, res, next) => {
-    console.error(`[${req.method}] /api${req.url}`);
-    return next();
+const db = new sqlite3.Database(path.resolve(__dirname, 'data.sqlite'));
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE IF NOT EXISTS stations (
+            ID                  INT,
+            Name                TEXT,
+            Lon                 FLOAT,
+            Lat                 FLOAT,
+            Wind_Speed_Legend   TEXT,
+            Wind_Dir_Legend     TEXT
+        );
+    `);
+
+    db.run(`
+        CREATE TABLE IF NOT EXISTS station_data (
+            Station_ID          INT,
+            Timestamp           INT,
+            Windspeed           FLOAT,
+            Wind_Direction      FLOAT,
+            Battery_Level       Float
+        );
+    `);
+
+    return main(db);
 });
 
-app.listen(4000, (err) => {
-    if (err) throw err;
+function main(db) {
+    router.use((req, res, next) => {
+        console.error(`[${req.method}] /api${req.url}`);
+        return next();
+    });
 
-    console.error('Server listening http://localhost:4000');
-});
+    /**
+     * Returns basic metadata about all stations
+     * managed in the database
+     */
+    router.get('/api/stations', (req, res) => {
+
+    });
+
+    router.post('/api/station', (req, res) => {
+
+    });
+
+    app.listen(4000, (err) => {
+        if (err) throw err;
+
+        console.error('Server listening http://localhost:4000');
+    });
+}
